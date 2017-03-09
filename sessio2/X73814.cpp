16d1b6bc165b80611f2148eace15fce41849e510
Roberto Ariosa <rob.ariosa@hotmail.com>
2017-03-09 13:27:02 +0100

    2.1.8
    Erase Estudiant::

--- sessio2/X73814.cpp
+++ sessio2/X73814.cpp
@@ -18,7 +18,7 @@ int main(){
   
   // Leer estudiantes
   for(int i = 0; i < nEst; ++i){
-    v[i].Estudiant::llegir();
+    v[i].llegir();
   }
   
   // Crear vector de salida
@@ -29,21 +29,21 @@ int main(){
   for(int i = 0; i < nEst; ++i){
     
     // Si el estudiante es igual al anterior (exceptuando el primer estudiante)
-    if(i != 0 and v[i].Estudiant::consultar_DNI() == v[i-1].Estudiant::consultar_DNI()){
+    if(i != 0 and v[i].consultar_DNI() == v[i-1].consultar_DNI()){
       
       // Si ambos tienen nota, modificar el final
-      if(v[i].Estudiant::te_nota() and vfinal[u-1].Estudiant::te_nota() and v[i].Estudiant::consultar_nota() > vfinal[u-1].Estudiant::consultar_nota()){
-        vfinal[u-1].Estudiant::modificar_nota(v[i].Estudiant::consultar_nota());
+      if(v[i].te_nota() and vfinal[u-1].te_nota() and v[i].consultar_nota() > vfinal[u-1].consultar_nota()){
+        vfinal[u-1].modificar_nota(v[i].consultar_nota());
         
         // Si sólo el primero tiene nota nota, añadírsela al segundo
-      } else if(v[i].Estudiant::te_nota() and !vfinal[u-1].Estudiant::te_nota()) {
-        vfinal[u-1].Estudiant::afegir_nota(v[i].Estudiant::consultar_nota());
+      } else if(v[i].te_nota() and !vfinal[u-1].te_nota()) {
+        vfinal[u-1].afegir_nota(v[i].consultar_nota());
       }
       
       // Si el estudiante no es igual al anterior, añadir al estudiante con su nota (si es que tiene)
     } else {
-      vfinal[u] = Estudiant(v[i].Estudiant::consultar_DNI());
-      if(v[i].Estudiant::te_nota()) vfinal[u].Estudiant::afegir_nota(v[i].Estudiant::consultar_nota());
+      vfinal[u] = Estudiant(v[i].consultar_DNI());
+      if(v[i].te_nota()) vfinal[u].afegir_nota(v[i].consultar_nota());
       ++u;
     }
   }
@@ -53,7 +53,7 @@ int main(){
   bool finish = false;
   // Escribir estudiantes hasta que se acabe el vector o hasta que se acaben los estudiantes
   while(i < nEst and !finish){
-    if(vfinal[i].Estudiant::consultar_DNI() != 0) vfinal[i].Estudiant::escriure();
+    if(vfinal[i].consultar_DNI() != 0) vfinal[i].escriure();
     else finish = true;
     ++i;
   }
