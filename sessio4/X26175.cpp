ab26e0c7aef5f2a9a147476df6d7978395342a3b
Roberto Ariosa <rob.ariosa@hotmail.com>
2017-03-17 12:47:23 +0100

    Revert "Update X26175.cpp"
    This reverts commit 4d1c32b9b7e25ca95ae6366f602345c1fa2bd4bc.

--- sessio4/X26175.cpp
+++ sessio4/X26175.cpp
@@ -7,31 +7,36 @@
 //
 
 #include "Cjt_estudiants.hh"
+#include <algorithm>
+
+bool comp(Estudiant a, Estudiant b){
+  return a.consultar_DNI() < b.consultar_DNI();
+}
 
 void Cjt_estudiants::afegir_estudiant(const Estudiant &est, bool& b)
 {
   int dni = est.consultar_DNI();
   int pos = cerca_dicot(vest, 0, nest-1, dni);
-  if(vest[pos].consultar_DNI() != dni){
+  if(vest[pos].consultar_DNI() != est.consultar_DNI()){
     b = false;
-
+    
     for(int i = nest-1; i >= pos; --i){
       vest[i+1] = vest[i];
     }
-
+    
     ++nest;
     vest[pos] = est;
-
+    
+    int nota = -1;
     if(est.te_nota()){
-      int nota = est.consultar_nota();
-      if(nota == 10){
-        ++intervals[9];
-      } else {
-        ++intervals[nota];
-      }
+      nota = est.consultar_nota();
     }
-
-
+    
+    if(nota == 10){
+      ++intervals[9];
+    } else if(nota != -1){
+      ++intervals[int(nota)];
+    }
   } else {
     b = true;
   }
@@ -42,22 +47,24 @@ void Cjt_estudiants::esborrar_estudiant(int dni, bool& b)
   int i = cerca_dicot(vest, 0, nest-1, dni);
   if(vest[i].consultar_DNI() == dni){
     b = true;
-
+    
     // Arreglar intervals
     if(vest[i].te_nota()){
       int nota = vest[i].consultar_nota();
-
+      
       if(nota == 10){
         --intervals[9];
       } else {
-        --intervals[nota];
+        --intervals[int(nota)];
       }
     }
-
+    
     // Borrar estudiante
-    for(int j = i; j < nest-1; ++j) vest[j] = vest[j+1];
+    vest[i] = vest[nest-1];
     --nest;
-
+    
+    sort(vest.begin(), vest.begin()+nest, comp);
+    
   } else {
     b = false;
   }
